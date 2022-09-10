7b58cb98159c0fa307fd12fa72195c65bd552205
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-09-10 15:48:08 +0600

    cartSummary page table design all done

--- src/Components/CartRow.js
+++ src/Components/CartRow.js
@@ -27,7 +27,8 @@ const CartRow = ({product}) => {
                 }
             </th>
             <th>
-                <div className="p-2 border flex justify-between text-xl">
+                <div className="flex justify-between items-center w-full">
+                <div className="p-2 border flex justify-between text-xl rounded-3xl w-[50%]">
                     <button onClick={decreamentQuantity}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
@@ -36,11 +37,21 @@ const CartRow = ({product}) => {
                     </button>
                     <span>{quantity}</span>
                     <button onClick={()=>setQuantity(1+quantity)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                     </button>
                 </div>
+                <div className="flex justify-between w-[50%]">
+               <div className="py-2 px-4 rounded-md ml-2 bg-gray-400 text-white">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+               </div>
+                <input type="checkbox"  className="checkbox checkbox-lg" />
+                </div>
+                </div>
+
             </th>
           </tr>
   )
