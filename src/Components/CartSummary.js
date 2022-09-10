4fad443e7a5bea1a7507f069264b1ad8115165d3
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-09-10 17:37:36 +0600

    today filtering function all

--- src/Components/CartSummary.js
+++ src/Components/CartSummary.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import useProducts from '../hook/useProducts'
 import  CartRow  from './CartRow';
 
 export default function CartSummary() {
-  const{products,loading}=useProducts();
+  const{products,loading, refech, setRefech}=useProducts();
+  const [filteringC,setFilteringC]=useState(null)
+  const [filteringS,setFilteringS]=useState(null)
   if(loading){
     return <h2>Loading data...</h2>
   }
-  const category =["Men's Sneaker", "Men's Pants", "Men's Boot","Cap"]
-  const getCategory=()=>{
+  const categoryName =["Men's Sneaker", "Men's Pants", "Men's Boot","Cap"]
+  // size filtering function 
+  const getSize=(e)=>{
+    if(filteringC){
+      const sizeFilter= filteringC.filter(product=>product.size ===e.target.value);
+      setFilteringS(sizeFilter)
+    }  
+  }
+// category fileter function 
+  const getCategory=(e)=>{
+      const categoryFilter = products.filter(product => product.category ===e.target.value)
+      setFilteringS(null)  
+      setFilteringC(categoryFilter)
 
   }
+  const getResetFilter  = ()=>{
+    setFilteringC(null)
+    setFilteringS(null)
+    setRefech(!refech)
+  }
+  console.log("category",filteringC);
+  console.log("size",filteringS);
   return (
     <div className='my-6 bg-light mx-2'>
       <h2 className='text-2xl my-4'>Order Summary</h2>
@@ -20,16 +40,16 @@ export default function CartSummary() {
           {/* Category filltering  */}
             <form className="flex justify-around w-full">
               <div className="form-control ">
-                <select onChange={getCategory} className="select select-bordered">
-                  <option  selected>Select Category</option>
+                <select onChange={getCategory}  className="select select-bordered">
+                 <option selected disabled >Select Category</option>
                  {
-                  category.map((ca,index)=> <option ca={ca} index={index}>{ca}</option>)
+                  categoryName?.map((ca,index)=> <option  key={index}>{ca}</option>)
                  }
                 </select>
               </div>
-              <div className="form-control w-24">
-                <select className="select select-bordered">
-                  <option  selected>Select Size</option>
+              <div className="form-control w-32">
+                <select disabled={!filteringC} onChange={getSize} className="select select-bordered">
+                  <option selected disabled>Select Size</option>
                   <option>XL</option>
                   <option>XXL</option>
                   <option>M</option>
@@ -41,12 +61,15 @@ export default function CartSummary() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
               </svg>
 
-                <button type="reset" className="py-2 px-4 bg-transparent ">Reset</button>
+                <button type="reset" onClick={getResetFilter} className="py-2 px-4 bg-transparent ">Reset</button>
               </div>
           </form>
         </div>
         <div className="search-filtering flex justify-end  w-[50%]">
-        <input type="text" placeholder="Type here"  className="input input-bordered input-accent w-[200px] mr-4" />
+        <div class="flex">
+          <span>Search:</span>
+          <input type="text" placeholder="Type here"  className="input input-bordered input-accent w-[200px] mr-4" />
+        </div>
         <button class="py-2 px-4 bg-blue-500 text-white">Add to Cart</button>
         </div>
       </div>
@@ -67,8 +90,14 @@ export default function CartSummary() {
         </thead>
         <tbody>
           {/* <!-- row 1 --> */}
-          {
+          { !filteringC &&
             products?.map(product=> <CartRow product={product} key={product?._id}/>)
+          }
+          {
+            filteringC && !filteringS && filteringC?.map(product=> <CartRow product={product} key={product?._id}/>)
+          }
+          {
+            filteringS && filteringS?.map(product=> <CartRow product={product} key={product?._id}/>)
           }
           </tbody>
       </table>
--- src/hook/useProducts.js
+++ src/hook/useProducts.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 
 const useProducts= ()=>{
     const [products,setProducts]=useState(null);
-    const[loading,setLoading]=useState(true)
+    const[loading,setLoading]=useState(true);
+     const [refech, setRefech]=useState(false)
     useEffect(()=>{
         fetch("products.json")
         .then(res=>res.json())
@@ -10,9 +11,9 @@ const useProducts= ()=>{
             setProducts(data)
             setLoading(false)
         })
-    },[])
+    },[refech])
 
-    return{products,loading}
+    return{products,loading, refech, setRefech}
 
 }
 export default useProducts;
